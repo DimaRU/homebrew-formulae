4e0fa872f53d4088a1839b2415d717a51c99de9c
Dmitriy Borovikov <dmitry@borovikov.name>
2024-02-19 20:21:36 +0300

    Add msg2swift formula

---
+++ Formula/msg2swift.rb
@@ -0,0 +1,24 @@
+class msg2swift < Formula
+  desc "Generate Swift model for ROS message file."
+  homepage "https://github.com/DimaRU/Msg2swift"
+  url "https://github.com/DimaRU/Msg2swift.git"
+  license "MIT"
+  head "https://github.com/DimaRU/Msg2swift.git", branch: "master", shallow: false
+
+  depends_on :xcode => "14.3"
+
+  def install
+    system "swift", "build",
+           "-c", "release",
+           "--disable-sandbox",
+           "--build-path", buildpath.to_s
+
+    system "install", "-d", "#{prefix}/bin"
+    system "install", buildpath/"release/msg2swift", "#{prefix}/bin"
+
+  end
+
+  test do
+    system "#{bin}/msg2swift", "--help"
+  end
+end
--- README.md
+++ README.md
@@ -8,7 +8,8 @@ $ brew tap DimaRU/formulae
 
 - **[xczip](https://github.com/DimaRU/xczip)**:
   Create xcframework zip archive for Swift binary package.
-
+- **[msg2swift](https://github.com/DimaRU/Msg2swift)**:
+  Generate Swift model for ROS message file.
 ## License
 
 [The MIT License](http://opensource.org/licenses/MIT)
